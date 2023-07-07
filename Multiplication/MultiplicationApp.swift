f58802659fb8606d5669d73b9857302d1c56e5cd
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2023-07-07 23:38:54 +0900

    Initial Commit

---
+++ Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by Yoonje Lee on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
